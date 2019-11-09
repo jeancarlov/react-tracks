08200d214492f2d900c5fae00d1a8bc829221f4d
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro-3.local>
2019-11-08 20:27:02 -0500

    tracks app created, models created in tracks.

---
+++ app/tracks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Track(models.Model):
+    #id
+    
+    title = models.CharField(max_length=50)
+
+    description = models.TextField(blank=True)
+
+    url = models.URLField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
